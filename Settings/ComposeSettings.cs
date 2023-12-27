b983cab05aaf3088fe187f557782bb88159da880
Edmond Baloku Vigre <edmond.vigre@bouvet.no>
2023-12-27 10:46:44 +0100

    updated solution_path option

--- Settings/ComposeSettings.cs
+++ Settings/ComposeSettings.cs
@@ -9,8 +9,8 @@ namespace CLI.Settings
         public static readonly List<string> DefaultValue = ["dashboard", "placement", "zipkin"];
 
         public string CommandName => "gen_compose";
-        public string CommandDescription => "Generates docker-compose.yml from your Dapr directory or for the specified service(s).";
-        public string CommandExample => "\n\nExamples:\n  dotnet run gen_compose --components rabbitmq sentry\n  dotnet run gen_compose --settings mtls https --services ServiceA\n  dotnet run gen_compose --solution_path ../../Backend\n  dotnet run gen_compose --components rabbitmq sentry --services Frontend Backend --solution_path ../../Backend";
+        public string CommandDescription => "Generates docker-compose.yml from your .Net solution(s) or for specified service(s).";
+        public string CommandExample => "\n\nExamples:\n  dotnet run gen_compose --components rabbitmq sentry\n  dotnet run gen_compose --settings mtls https --services ServiceA\n  dotnet run gen_compose --solution_paths ../../Backend\n  dotnet run gen_compose --components rabbitmq sentry --services Frontend Backend --solution_paths ../../Backend";
 
         public static string[] SettingOptionName { get; set; } = ["--s", "--settings"];
         public static string SettingOptionDescription { get; set; } = "Additional settings for your services.";
@@ -18,18 +18,19 @@ namespace CLI.Settings
         public static string ServiceOptionDescription { get; set; } = "The specific service(s) to generate docker-compose content to.";
         public static string[] ComponentOptionName { get; set; } = ["--c", "--components"];
         public static string ComponentOptionDescription { get; set; } = "The specific component(s) to generate docker-compose content to.";
-        public static string[] SolutionOptionName { get; set; } = ["--so", "--solution_path"];
+        public static string[] SolutionOptionName { get; set; } = ["--sp", "--solution_paths"];
         public static string SolutionOptionDescription { get; set; } = "The path to your .Net solution file (from executing path). Used to generate certificates for all services dependent on Dapr.";
 
         private static readonly Option<List<string>> ComponentOption = new(ComponentOptionName, () => DefaultValue, ComponentOptionDescription) { AllowMultipleArgumentsPerToken = true };
         private static readonly Option<List<string>> SettingOption = new(SettingOptionName, SettingOptionDescription) { AllowMultipleArgumentsPerToken = true };
         private static readonly Option<List<string>> ServiceOption = new(ServiceOptionName, ServiceOptionDescription) { AllowMultipleArgumentsPerToken = true };
-        private static readonly Option<List<string>> SolutionOption = new(SolutionOptionName, SolutionOptionDescription);
+        private static readonly Option<List<string>> SolutionOption = new(SolutionOptionName, SolutionOptionDescription) { AllowMultipleArgumentsPerToken = true };
         public List<Option<List<string>>> Options { get; set; } = [ComponentOption, SettingOption, ServiceOption, SolutionOption];
 
         public ComposeSettings()
         {
             ComponentOption.FromAmong([.. AvailableComponents]);
+            SettingOption.FromAmong([.. AvailableSettings]);
         }
     }
 }
--- Settings/DockerfileSettings.cs
+++ Settings/DockerfileSettings.cs
@@ -6,15 +6,15 @@ namespace CLI.Settings
     {
         public string CommandName => "gen_dockerfiles";
         public string CommandDescription => "Generates dockerfiles for all your projects in the specified solutions.";
-        public string CommandExample => "\n\nExamples:\n  dotnet run gen_dockerfiles --solution_path ../../Service1 ../../Service2";
+        public string CommandExample => "\n\nExamples:\n  dotnet run gen_dockerfiles --solution_paths ../../Service1 ../../Service2";
 
         public static string[] ProjectOptionName { get; set; } = ["--pp", "--project_path"];
         public static string ProjectOptionDescription { get; set; } = "The path to the root of your project (from executing path). Not needed if it's a git project. Used to find correct paths.";
-        public static string[] SolutionOptionName { get; set; } = ["--sp", "--solution_path"];
+        public static string[] SolutionOptionName { get; set; } = ["--sp", "--solution_paths"];
         public static string SolutionOptionDescription { get; set; } = "The path to your .Net solution file (from executing path). Used to generate dockerfile for all projects in the sln file.";
 
         private static readonly Option<List<string>> ProjectOption = new(ProjectOptionName, ProjectOptionDescription);
-        private static readonly Option<List<string>> SolutionOption = new(SolutionOptionName, SolutionOptionDescription);
+        private static readonly Option<List<string>> SolutionOption = new(SolutionOptionName, SolutionOptionDescription) { AllowMultipleArgumentsPerToken = true };
         public List<Option<List<string>>> Options { get; set; } = [ProjectOption, SolutionOption];
     }
 }
