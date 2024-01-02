ba52c4af288d9d75c665263bdf82b042221cec27
Edmond Baloku Vigre <edmond.vigre@bouvet.no>
2024-01-02 11:09:15 +0100

    changed config template to use handlebars

--- Services/Commands/ConfigService.cs
+++ Services/Commands/ConfigService.cs
@@ -13,32 +13,13 @@ namespace CLI.Services
         protected override List<string> CreateFiles(OptionDictionary options, IPath workingDir)
         {
             Key settingKey = new("settings");
-            string config = _templateFactory.CreateTemplate<ConfigTemplate>();
+            ConfigTemplate configService = _templateFactory.GetTemplateService<ConfigTemplate>();
             OptionValues settingOpt = options.GetAllPairValues(settingKey);
 
-            foreach (var argument in settingOpt.GetValues())
-            {
-                config = GetArgumentTemplate(argument, config);
-            }
+            string config = configService.Render(settingOpt);
 
-            config = PlaceholderRegex().Replace(config, string.Empty);
             DirectoryService.WriteFile(workingDir, CONFIG_YAML, config);
             return ["config"];
-        }
-
-        protected override string GetArgumentTemplate(Value argument, string template)
-        {
-            string argTemplate = argument.ToString().ToLower() switch
-            {
-                "logging" => _templateFactory.CreateTemplate<LoggingTemplate>(),
-                "metric" => _templateFactory.CreateTemplate<MetricTemplate>(),
-                "middleware" => _templateFactory.CreateTemplate<MiddlewareTemplate>(),
-                "mtls" => _templateFactory.CreateTemplate<MTlsTemplate>(),
-                "tracing" => _templateFactory.CreateTemplate<TracingTemplate>(),
-                _ => throw new ArgumentException("Invalid setting name:" + argument, nameof(argument))
-            };
-
-            return template.Replace($"{{{{{argument}}}}}", argTemplate);
         }
     }
 }
--- Templates/Configs/ConfigTemplate.cs
+++ Templates/Configs/ConfigTemplate.cs
@@ -1,20 +1,62 @@
+using CLI.Models;
+
 namespace CLI.Templates
 {
-  public class ConfigTemplate : TemplateBase
+  public class ConfigTemplate(TemplateFactory templateFactory) : TemplateBase
   {
+    private readonly TemplateFactory _templateFactory = templateFactory;
     protected override string TemplateString =>
 @"apiVersion: dapr.io/v1alpha1
 kind: Configuration
 metadata:
   name: daprsystem
 spec:
-  {{logging}}
-  {{metric}}
-  {{middleware}}
-  {{mtls}}
-  {{tracing}}
+  {{{logging}}}
+  {{{metric}}}
+  {{{middleware}}}
+  {{{mtls}}}
+  {{{tracing}}}
 
 # For more information: https://docs.dapr.io/operations/configuration/configuration-overview/
 ";
+
+    public string Render(OptionValues settings)
+    {
+      var data = new
+      {
+        logging = GetSettingTemplate("logging", settings),
+        metric = GetSettingTemplate("metric", settings),
+        middleware = GetSettingTemplate("middleware", settings),
+        mtls = GetSettingTemplate("mtls", settings),
+        tracing = GetSettingTemplate("tracing", settings),
+      };
+
+      string result = _template(data);
+      result = PlaceholderRegex().Replace(result, string.Empty);
+
+      return result;
+    }
+
+    private string GetSettingTemplate(string settingName, OptionValues settings)
+    {
+      if (settings.GetStringEnumerable().Contains(settingName))
+      {
+        string argTemplate = settingName.ToLower() switch
+        {
+          "logging" => _templateFactory.CreateTemplate<LoggingTemplate>(),
+          "metric" => _templateFactory.CreateTemplate<MetricTemplate>(),
+          "middleware" => _templateFactory.CreateTemplate<MiddlewareTemplate>(),
+          "mtls" => _templateFactory.CreateTemplate<MTlsTemplate>(),
+          "tracing" => _templateFactory.CreateTemplate<TracingTemplate>(),
+          _ => throw new ArgumentException("Invalid setting name:" + settingName, nameof(settingName))
+        };
+
+        return argTemplate;
+      }
+      else
+      {
+        return "{{}}";
+      }
+    }
   }
 }
