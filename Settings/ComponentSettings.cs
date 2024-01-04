5f24c747875d0c2248c8e7183428ee5e96d934c8
Edmond Baloku Vigre <edmond.vigre@bouvet.no>
2024-01-04 18:33:46 +0100

    updated command and cli descriptions

--- Settings/ComponentSettings.cs
+++ Settings/ComponentSettings.cs
@@ -8,11 +8,11 @@ namespace Daprify.Settings
         public static List<string> DefaultValue { get; set; } = ["pubsub", "statestore"];
 
         public string CommandName => "gen_components";
-        public string CommandDescription => "Generates the desired Dapr components YAML files.";
+        public string CommandDescription => "Generates the desired Dapr components .yml files.";
         public string CommandExample => "\n\nExamples:\n  dotnet run gen_components --components statestore\n  dotnet run gen_components --components statestore pubsub";
 
         public static string[] OptionName { get; set; } = ["--c", "--components"];
-        public static string OptionDescription { get; set; } = "The specific component(s) to generate config files for.";
+        public static string OptionDescription { get; set; } = "The specific component(s) to generate a file for.";
 
         private static readonly Option<List<string>> ComponentOption = new(OptionName, () => DefaultValue, OptionDescription) { AllowMultipleArgumentsPerToken = true };
         public List<Option<List<string>>> Options { get; set; } = [ComponentOption];
