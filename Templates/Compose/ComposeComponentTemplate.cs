e8b84afdcc3c52999eeaf670989d59028797c22f
Edmond Baloku Vigre <edmond.vigre@bouvet.no>
2024-01-05 13:30:54 +0100

    use or in pattern matching

--- Templates/Compose/ComposeComponentTemplate.cs
+++ Templates/Compose/ComposeComponentTemplate.cs
@@ -17,7 +17,6 @@ namespace Daprify.Templates
             OptionValues componentOpt = options.GetAllPairValues(componentKey);
 
             var componentTemplates = componentOpt.GetValues()
-                .Select(PreProcessArgument)
                 .Select(GetArgumentTemplate)
                 .Where(template => !string.IsNullOrEmpty(template));
 
@@ -30,24 +29,14 @@ namespace Daprify.Templates
             return _template(data);
         }
 
-        private static Value PreProcessArgument(Value argument)
-        {
-            return argument.ToString().ToLower() switch
-            {
-                "pubsub" => new Value("rabbitmq"),
-                "statestore" => new Value("redis"),
-                _ => argument
-            };
-        }
-
         private string GetArgumentTemplate(Value argument)
         {
             return argument.ToString().ToLower() switch
             {
                 "dashboard" => _templateFactory.CreateTemplate<DaprDashboardTemplate>(),
                 "placement" => _templateFactory.CreateTemplate<PlacementTemplate>(),
-                "rabbitmq" => _templateFactory.CreateTemplate<RabbitMqTemplate>(),
-                "redis" => _templateFactory.CreateTemplate<RedisTemplate>(),
+                "rabbitmq" or "pubsub" => _templateFactory.CreateTemplate<RabbitMqTemplate>(),
+                "redis" or "statestore" => _templateFactory.CreateTemplate<RedisTemplate>(),
                 "sentry" => _templateFactory.CreateTemplate<SentryTemplate>(),
                 "zipkin" => _templateFactory.CreateTemplate<ZipkinTemplate>(),
                 _ => string.Empty
